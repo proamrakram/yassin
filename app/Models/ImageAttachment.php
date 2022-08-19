639cf34c082cd637cd474b81c477d8e2893db92b
Amr Akram <amrprogrammingn@gmail.com>
2022-08-19 07:30:32 +0300

    ok

--- app/Models/ImageAttachment.php
+++ app/Models/ImageAttachment.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class ImageAttachment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'image_url',
+        'mime_type',
+        'hash_sha256',
+        'file_size',
+        'image_id',
+        'messaging_product',
+        'sender_image_message_id',
+    ];
+
+    public function senderMessage()
+    {
+        return $this->belongsTo(SenderImageMessages::class, 'sender_image_message_id', 'id');
+    }
+
 }
