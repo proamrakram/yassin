c5514fce2e9bade2e9f6a7fac79ddbcb2f235d34
Amr Akram <amrprogrammingn@gmail.com>
2022-08-31 13:16:35 +0300

    ok

--- resources/views/components/portfolio/portfolio.blade.php
+++ resources/views/components/portfolio/portfolio.blade.php
@@ -1,46 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
+<!-- Favicons -->
+<link href="{{ asset('whatsapp-assets/img/favicon.png') }}" rel="icon">
+<link href="{{ asset('whatsapp-assets/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
 
-<head>
-    <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+<!-- Google Fonts -->
+<link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet">
 
-    <title>MyResume Bootstrap Template - Index</title>
-    <meta content="" name="description">
-    <meta content="" name="keywords">
+<!-- Vendor CSS Files -->
+<link href="{{ asset('whatsapp-assets/vendor/aos/aos.css') }}" rel="stylesheet">
+<link href="{{ asset('whatsapp-assets/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+<link href="{{ asset('whatsapp-assets/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+<link href="{{ asset('whatsapp-assets/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+<link href="{{ asset('whatsapp-assets/assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+<link href="{{ asset('whatsapp-assets/assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
 
-    <!-- Favicons -->
-    <link href="{{ asset('whatsapp-assets/img/favicon.png') }}" rel="icon">
-    <link href="{{ asset('whatsapp-assets/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+<!-- Template Main CSS File -->
+<link href="{{ asset('whatsapp-assets/assets/css/style.css') }}" rel="stylesheet">
 
-    <!-- Google Fonts -->
-    <link
-        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
-        rel="stylesheet">
 
-    <!-- Vendor CSS Files -->
-    <link href="{{ asset('whatsapp-assets/vendor/aos/aos.css') }}" rel="stylesheet">
-    <link href="{{ asset('whatsapp-assets/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('whatsapp-assets/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-    <link href="{{ asset('whatsapp-assets/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('whatsapp-assets/assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('whatsapp-assets/assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
 
-    <!-- Template Main CSS File -->
-    <link href="{{ asset('whatsapp-assets/assets/css/style.css') }}" rel="stylesheet">
+<main>
 
-    <!-- =======================================================
-  * Template Name: MyResume - v4.8.1
-  * Template URL: https://bootstrapmade.com/free-html-bootstrap-template-my-resume/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
-</head>
-
-<body>
-
-    <!-- ======= Mobile nav tog
-        ======= Portfolio Section ======= -->
     <section id="portfolio" class="portfolio section-bg">
         <div class="container" data-aos="fade-up">
 
@@ -240,52 +221,24 @@
         </div>
     </section><!-- End Portfolio Section -->
 
-    </main><!-- End #main -->
+</main><!-- End #main -->
 
-    <!-- ======= Footer ======= -->
-    <footer id="footer">
-        <div class="container">
-            <h3>Brandon Johnson</h3>
-            <p>Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi
-                placeat.</p>
-            <div class="social-links">
-                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-                <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-            </div>
-            <div class="copyright">
-                &copy; Copyright <strong><span>MyResume</span></strong>. All Rights Reserved
-            </div>
-            <div class="credits">
-                <!-- All the links in the footer should remain intact. -->
-                <!-- You can delete the links only if you purchased the pro version. -->
-                <!-- Licensing information: [license-url] -->
-                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/free-html-bootstrap-template-my-resume/ -->
-                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-            </div>
-        </div>
-    </footer><!-- End Footer -->
 
-    <div id="preloader"></div>
-    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
-            class="bi bi-arrow-up-short"></i></a>
+<div id="preloader"></div>
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+        class="bi bi-arrow-up-short"></i></a>
 
-    <!-- Vendor JS Files -->
-    <script src="{{ asset('whatsapp-assets/assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
-    <script src="{{ asset('whatsapp-assets/assets/vendor/aos/aos.js') }}"></script>
-    <script src="{{ asset('whatsapp-assets/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('whatsapp-assets/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
-    <script src="{{ asset('whatsapp-assets/assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
-    <script src="{{ asset('whatsapp-assets/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
-    <script src="{{ asset('whatsapp-assets/assets/vendor/typed.js/typed.min.js') }}"></script>
-    <script src="{{ asset('whatsapp-assets/assets/vendor/waypoints/noframework.waypoints.js') }}"></script>
-    <script src="{{ asset('whatsapp-assets/assets/vendor/php-email-form/validate.js') }}"></script>
 
-    <!-- Template Main JS File -->
-    <script src="{{ asset('whatsapp-assets/assets/js/main.js') }}"></script>
+<!-- Vendor JS Files -->
+<script src="{{ asset('whatsapp-assets/assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
+<script src="{{ asset('whatsapp-assets/assets/vendor/aos/aos.js') }}"></script>
+<script src="{{ asset('whatsapp-assets/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('whatsapp-assets/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+<script src="{{ asset('whatsapp-assets/assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+<script src="{{ asset('whatsapp-assets/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+<script src="{{ asset('whatsapp-assets/assets/vendor/typed.js/typed.min.js') }}"></script>
+<script src="{{ asset('whatsapp-assets/assets/vendor/waypoints/noframework.waypoints.js') }}"></script>
+<script src="{{ asset('whatsapp-assets/assets/vendor/php-email-form/validate.js') }}"></script>
 
-</body>
-
-</html>
+<!-- Template Main JS File -->
+<script src="{{ asset('whatsapp-assets/assets/js/main.js') }}"></script>
