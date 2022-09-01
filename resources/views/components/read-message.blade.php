e615ce8168450fdd41beb2d05d6a41a543c3eacb
Amr Akram <amrprogrammingn@gmail.com>
2022-09-01 16:54:02 +0300

    update modal

--- resources/views/components/read-message.blade.php
+++ resources/views/components/read-message.blade.php
@@ -1,23 +1,6 @@
-<div class="modal fade" id="second{{ $message->id }}" aria-hidden="true" aria-labelledby="secondTogle{{ $message->id }}"
-    tabindex="-1">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="secondTogle{{ $message->id }}">Sending Replying Message</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <input type="text" value="Enter your message here to send it ot the user">
-            </div>
-            <div class="modal-footer">
-                <button class="btn btn-primary" data-bs-target="#main{{ $message->id }}"
-                    data-bs-toggle="modal">Submit</button>
-            </div>
-        </div>
-    </div>
-</div>
-
-
+<a class="btn btn-primary" data-bs-toggle="modal" href="#mainButton{{ $message->id }}" role="button"> <img
+        src="{{ asset('whatsapp-assets/svg/read-message.svg') }}" width="30" height="20" alt="read-message.svg">
+</a>
 
 <div class="modal fade" id="mainButton{{ $message->id }}" aria-hidden="true"
     aria-labelledby="mainTitle{{ $message->id }}" tabindex="-1">
@@ -57,6 +40,3 @@
         </div>
     </div>
 </div>
-<a class="btn btn-primary" data-bs-toggle="modal" href="#mainButton{{ $message->id }}" role="button"> <img
-        src="{{ asset('whatsapp-assets/svg/read-message.svg') }}" width="30" height="20"
-        alt="read-message.svg"></a>
