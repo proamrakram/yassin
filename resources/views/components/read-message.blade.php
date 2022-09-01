bf733da98fd64005d8f187f886acf9392cc92f58
Amr Akram <amrprogrammingn@gmail.com>
2022-09-01 17:18:26 +0300

    name card

--- resources/views/components/read-message.blade.php
+++ resources/views/components/read-message.blade.php
@@ -10,8 +10,8 @@
 
 
             <div class="modal-header">
-                <h5 class="modal-title" id="mainTitle{{ $message->id }}">Message from </h5>
-                <span>{{ $message->senderMessage->name }}</span>
+                <h5 class="modal-title" id="mainTitle{{ $message->id }}">Message from: </h5>
+                <span>   {{ $message->senderMessage->name }}</span>
 
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
