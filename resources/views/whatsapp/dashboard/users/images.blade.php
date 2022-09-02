c770714b011afca92fb10e06522da631ccbc34b0
Amr Akram <amrprogrammingn@gmail.com>
2022-09-02 12:22:53 +0300

    new update

--- resources/views/whatsapp/dashboard/users/images.blade.php
+++ resources/views/whatsapp/dashboard/users/images.blade.php
@@ -125,17 +125,15 @@
                     const listItem = document.createElement('li');
                     const para = document.createElement('p');
 
-                    console.log("Ok");
-
                     if (validFileType(file)) {
-                        console.log(file, preview, list, validFileType(file));
+                        console.log(preview, validFileType(file));
 
                         para.textContent = `File name ${file.name}, file size ${returnFileSize(file.size)}.`;
                         // const image = document.createElement('img');
                         // image.src = URL.createObjectURL(file);
-
                         // listItem.appendChild(image);
-                        // listItem.appendChild(para);
+
+                        listItem.appendChild(para);
                     } else {
                         para.textContent = `File name ${file.name}: Not a valid file type. Update your selection.`;
                         listItem.appendChild(para);
