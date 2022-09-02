92198aea24ec56e81b6f7d6f90be942473753430
Amr Akram <amrprogrammingn@gmail.com>
2022-09-02 10:02:27 +0300

    ok

--- app/Http/Controllers/Bot/BotController.php
+++ app/Http/Controllers/Bot/BotController.php
@@ -65,6 +65,7 @@ class BotController extends Controller
     {
         $message_body = ['preview_url' => false, 'body' => $request->message_reply,];
         $result = $this->reply($this->headers, 'text', $wa_user, $message_body, $wa_message_id);
+        return $result;
         if (!$result) {
             return redirect()->back()->with('success', 'Message has not been sent successfully!!');
         }
