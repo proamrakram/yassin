d1b65e4801b7bf6369b673a2bfbe26c55ce8420b
Misser <amrprogrammingn@gmail.com>
2022-09-02 22:20:44 +0300

    ok

--- app/Http/Controllers/WhatsAppController.php
+++ app/Http/Controllers/WhatsAppController.php
@@ -41,13 +41,14 @@ class WhatsAppController extends Controller
 
         Storage::disk('local')->put('whatsappdata.txt', print_r($data, true));
 
-        if ($this->value->contacts && $bot) {
-            $sender_whats_app = $this->saveSenderWhatsApp($bot, $this->value->contacts[0]);
-            if (!$sender_whats_app) {
-                return error_log('Sender not found');
+        if (isset($this->value)) {
+            if (isset($this->value->contacts) && $bot) {
+                $sender_whats_app = $this->saveSenderWhatsApp($bot, $this->value->contacts[0]);
+                if (!$sender_whats_app) {
+                    return error_log('Sender not found');
+                }
             }
         }
-
         if ($this->value->messages && $this->value->messages[0]->type == 'text') {
             $sender_text_message = $this->saveSenderTextMessages($sender_whats_app, $this->value->messages[0]);
         }
