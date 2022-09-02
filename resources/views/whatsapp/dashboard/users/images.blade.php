c47d9c3e0af9388e4c3cd531dedf01ee108e499d
Amr Akram <amrprogrammingn@gmail.com>
2022-09-02 12:04:25 +0300

    new update

--- resources/views/whatsapp/dashboard/users/images.blade.php
+++ resources/views/whatsapp/dashboard/users/images.blade.php
@@ -85,8 +85,57 @@
                 const para = document.createElement('p');
                 para.textContent = 'No image file currently selected for upload';
                 preview.appendChild(para);
+            } else {
+
+                const list = document.createElement('ol');
+
+                preview.appendChild(list);
+
+                for (const file of curFiles) {
+                    const listItem = document.createElement('li');
+                    const para = document.createElement('p');
+                    if (validFileType(file)) {
+                        para.textContent = `File name ${file.name}, file size ${returnFileSize(file.size)}.`;
+                        const image = document.createElement('img');
+                        image.src = URL.createObjectURL(file);
+
+                        listItem.appendChild(image);
+                        listItem.appendChild(para);
+                    } else {
+                        para.textContent = `File name ${file.name}: Not a valid file type. Update your selection.`;
+                        listItem.appendChild(para);
+                    }
+
+                    list.appendChild(listItem);
+                }
             }
 
+            const fileTypes = [
+                "image/apng",
+                "image/bmp",
+                "image/gif",
+                "image/jpeg",
+                "image/pjpeg",
+                "image/png",
+                "image/svg+xml",
+                "image/tiff",
+                "image/webp",
+                "image/x-icon"
+            ];
+
+            function validFileType(file) {
+                return fileTypes.includes(file.type);
+            }
+
+            function returnFileSize(number) {
+                if (number < 1024) {
+                    return `${number} bytes`;
+                } else if (number >= 1024 && number < 1048576) {
+                    return `${(number / 1024).toFixed(1)} KB`;
+                } else if (number >= 1048576) {
+                    return `${(number / 1048576).toFixed(1)} MB`;
+                }
+            }
         }
     </script>
 
