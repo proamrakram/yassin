9b202a40c43f3f0d1305a60274fd823c59579b22
Misser <amrprogrammingn@gmail.com>
2022-09-03 00:05:28 +0300

    ok

--- app/Http/Controllers/Bot/BotController.php
+++ app/Http/Controllers/Bot/BotController.php
@@ -109,6 +109,7 @@ class BotController extends Controller
 
     public function sendImageMessageByURL($url, $wa_user, $path)
     {
+        dd($url);
         $message_body = ['link' => $url];
         $result = $this->send($this->headers, 'image', $wa_user, $message_body);
 
