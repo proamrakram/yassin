c92f28ddb268bd5b38dd1873bdefd7a70848b9a1
Misser <amrprogrammingn@gmail.com>
2022-09-02 22:55:21 +0300

    ok

--- resources/views/components/portfolio.blade.php
+++ resources/views/components/portfolio.blade.php
@@ -10,7 +10,7 @@
     @endif
 
 
-    <div class="row row-cols-1 row-cols-md-2 g-4">
+    <div class="row row-cols-1 row-cols-md-3 g-4">
 
         @foreach ($wauser->imageMessages as $image)
             <div class="col">
