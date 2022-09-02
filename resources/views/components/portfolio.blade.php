d5d1143ddc6f8cc564935c8740e26d4a641eaa06
Misser <amrprogrammingn@gmail.com>
2022-09-02 22:42:58 +0300

    ok

--- resources/views/components/portfolio.blade.php
+++ resources/views/components/portfolio.blade.php
@@ -15,7 +15,7 @@
         @foreach ($wauser->imageMessages as $image)
             <div class="col">
                 <div class="card h-100">
-                    <img src="{{asset('storage/') .  $image->imageAttachment->image_url}}" class="card-img-top"
+                    <img src="{{asset('storage') . '/' .  $image->imageAttachment->image_url}}" class="card-img-top"
                         style="width:100% ; height:15vw ; object-fit:cover;" alt="...">
                     <div class="card-body  d-flex flex-column">
                         <span class="card-title"><strong>Name: </strong>{{ '(' . $image->senderMessage->name . ')' }}</span>
