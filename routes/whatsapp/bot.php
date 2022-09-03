c36573f0e65e4952f7e4b4fe50246c43f61f01c6
Misser <MiasserKuhail@yahoo.com>
2022-09-04 02:30:06 +0300

    ok

--- routes/whatsapp/bot.php
+++ routes/whatsapp/bot.php
@@ -26,5 +26,8 @@ Route::controller(BotController::class)->prefix('bot')->as('bot.')->group(functi
     Route::get('send-imag-message-by-id/{wa_user}/{wa_image_id}', 'sendImageMessagebyID')->name('send-imag-message-by-id');
 
     Route::get('get-all-message-templates', 'getAllMessageTemplate')->name('get-all-message-template');
+    Route::get('get-message-template', 'getMessageTemplate')->name('get-message-template');
+
+
     Route::get('send-message-template-text/{wa_user}', 'sendMessageTemplateText')->name('send-message-template-text');
 });
