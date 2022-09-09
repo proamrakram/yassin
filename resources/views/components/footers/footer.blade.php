a204e697781bba608ef7b472c9a9d81f788b04ec
Misser <MiasserKuhail@yahoo.com>
2022-09-09 09:14:32 +0300

    ok

--- resources/views/components/footers/footer.blade.php
+++ resources/views/components/footers/footer.blade.php
@@ -33,6 +33,64 @@
 <script src="{{ asset('assets/js/toastr.min.js') }}"></script>
 
 <script>
+    // Display an info toast with no title
+    // toastr.info('Are you the 6 fingered man?')
+    // // Display a warning toast, with no title
+    // toastr.warning('My name is Inigo Montoya. You killed my father, prepare to die!')
+    // // Display a success toast, with a title
+    // toastr.success('Have fun storming the castle!', 'Miracle Max Says')
+    // // Display an error toast, with a title
+    // toastr.error('I do not think that word means what you think it means.', 'Inconceivable!')
+    // // Immediately remove current toasts without using animation
+    // toastr.remove()
+    // // Remove current toasts using animation
+    // toastr.clear()
+    // // Override global options
+    // toastr.success('We do have the Kapua suite available.', 'Turtle Bay Resort', {
+    //     timeOut: 5000
+    // })
+    // toastr.options.escapeHtml = true;
+    // toastr.options.closeButton = true;
+    // toastr.options.closeHtml = '<button><i class="icon-off"></i></button>';
+    // toastr.options.closeMethod = 'fadeOut';
+    // toastr.options.closeDuration = 300;
+    // toastr.options.closeEasing = 'swing';
+    // toastr.options.newestOnTop = false;
+
+    // Define a callback for when the toast is shown/hidden/clicked
+    // toastr.options.onShown = function() {
+    //     console.log('hello');
+    // }
+    // toastr.options.onHidden = function() {
+    //     console.log('goodbye');
+    // }
+    // toastr.options.onclick = function() {
+    //     console.log('clicked');
+    // }
+    // toastr.options.onCloseClick = function() {
+    //     console.log('close button clicked');
+    // }
+
+    // toastr.options.showEasing = 'swing';
+    // toastr.options.hideEasing = 'linear';
+    // toastr.options.closeEasing = 'linear';
+    // toastr.options.showEasing = 'easeOutBounce';
+    // toastr.options.hideEasing = 'easeInBack';
+    // toastr.options.closeEasing = 'easeInBack';
+    // toastr.options.showMethod = 'slideDown';
+    // toastr.options.hideMethod = 'slideUp';
+    // toastr.options.closeMethod = 'slideUp';
+    // toastr.options.preventDuplicates = true;
+    // toastr.options.timeOut = 30; // How long the toast will display without user interaction
+    // toastr.options.extendedTimeOut = 60; // How long the toast will display after a user hovers over it
+    // toastr.options.timeOut = 0;
+    // toastr.options.extendedTimeOut = 0;
+    // toastr.options.progressBar = true;
+    // toastr.options.rtl = true;
+
+    // Show at the bottom, top is the default.
+    toastr.options.newestOnTop = false;
+
     // Toaster Response
     @if (Session::has('success'))
         toastr.success("{{ Session::get('success') }}");
