ac2d2ecf1a9dbc9a7ebd7c070c0bd028451f40b8
Misser <MiasserKuhail@yahoo.com>
2022-09-09 23:14:14 +0300

    ok

--- app/Http/Traits/TemplateMessages.php
+++ app/Http/Traits/TemplateMessages.php
@@ -60,8 +60,9 @@ trait TemplateMessages
     public function setHeaderMessageTemplate($request)
     {
         return [
-            "type" => "image",
-            "image" => [
+            "type" => "header",
+            "format" => "image",
+            "text" => [
                 'link' => "https://static.remove.bg/remove-bg-web/37843dee2531e43723b012aa78be4b91cc211fef/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg"
             ]
         ];
