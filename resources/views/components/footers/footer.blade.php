006362700a89a29bfc6fe8a11edbe522cccbc8e5
Misser <MiasserKuhail@yahoo.com>
2022-09-09 09:50:21 +0300

    ok

--- resources/views/components/footers/footer.blade.php
+++ resources/views/components/footers/footer.blade.php
@@ -99,7 +99,7 @@
         "preventDuplicates": false,
         "onclick": null,
         "showDuration": "300",
-        "hideDuration": "1000",
+        "hideDuration": "3000",
         "timeOut": "5000",
         "extendedTimeOut": "1000",
         "showEasing": "swing",
