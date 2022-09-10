ed922a4da194253241caf6430bf8147930cdf5b2
Misser <MiasserKuhail@yahoo.com>
2022-09-10 08:22:46 +0300

    ok

--- app/Http/Traits/TemplateMessages.php
+++ app/Http/Traits/TemplateMessages.php
@@ -63,8 +63,9 @@ trait TemplateMessages
             "type" => "header",
             "format" => "IMAGE",
             "example" => [
-                "header_url" => "https://static.remove.bg/remove-bg-web/37843dee2531e43723b012aa78be4b91cc211fef/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg"
-
+                "header_handle" => [
+                    "https://static.remove.bg/remove-bg-web/37843dee2531e43723b012aa78be4b91cc211fef/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg"
+                ]
             ]
         ];
     }
