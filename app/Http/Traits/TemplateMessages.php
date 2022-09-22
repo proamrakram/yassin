ec145a498e5214a3e919acb20b6cb1332aa0058b
Misser <MiasserKuhail@yahoo.com>
2022-09-22 17:12:16 +0300

    ok

--- app/Http/Traits/TemplateMessages.php
+++ app/Http/Traits/TemplateMessages.php
@@ -72,8 +72,10 @@ trait TemplateMessages
                 "type" => "header",
                 "format" => "IMAGE",
                 "example" => [
-                    "header_handle" => ["https://media.geeksforgeeks.org/wp-content/uploads/20200205203241/gfg30.jpg"],
-                    // "header_url" => ["https://media.geeksforgeeks.org/wp-content/uploads/20200205203241/gfg30.jpg"]
+                    "header_handle" => ["https://tgtgreenteknoloji.com/whatsapp-assets/img/avatar-0.jpg"],
+                    "header_text" => ["Hello World"],
+                    "body_text" => [["Amr Akkram"]],
+                    // "header_url" => ["https://tgtgreenteknoloji.com/whatsapp-assets/img/avatar-0.jpg"]
                 ]
             ];
         }
