e4d5e736403693c4e5467a81c43030c491adfe54
Misser <MiasserKuhail@yahoo.com>
2022-09-22 17:31:38 +0300

    ok

--- app/Http/Controllers/Bot/BotController.php
+++ app/Http/Controllers/Bot/BotController.php
@@ -143,6 +143,19 @@ class BotController extends Controller
                 'language' => [
                     'code' => 'en_US'
                 ],
+
+                'components' => [
+                    [
+                        "type" => "header",
+                        "format" => "text",
+                        "text" => "Amr Akram is {{1}}"
+                    ],
+                    [
+                        "type" => "body",
+                        "text" => "We are now working on {{1}} platform"
+                    ],
+                ],
+
             ]
         ];
         $url =  "https://graph.facebook.com/v14.0/$bot/message_templates";
