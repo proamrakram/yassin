caf47d3b97c9ea8ef5049cf468c98667b3ad7ecc
Misser <MiasserKuhail@yahoo.com>
2022-09-23 20:03:38 +0300

    ok

--- app/Http/Controllers/Bot/BotController.php
+++ app/Http/Controllers/Bot/BotController.php
@@ -325,9 +325,9 @@ class BotController extends Controller
                 "interactive" => [
                     "type" => "button",
                     "header" => [
-                        "type" => "video",
-                        "video" => [
-                            "link" => "https://www.appsloveworld.com/wp-content/uploads/2018/10/Sample-Mp4-Videos.mp4",
+                        "type" => "image",
+                        "image" => [
+                            "link" => "https://cdn.statusqueen.com/dpimages/thumbnail/whatsapp%20dp%20image6-601.jpg",
                         ]
                     ],
                     "body" => [
