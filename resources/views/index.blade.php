97dc65f5b0f61d81997d047f806ac5e8ff806bc4
Misser <MiasserKuhail@yahoo.com>
2022-10-08 17:06:28 +0300

    ok

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -702,9 +702,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
 
-    {{-- <link rel="stylesheet" href="{{ asset('assets/css/bootstrap-grid.min.css') }}"> --}}
-    {{-- <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}"> --}}
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap-grid.min.css') }}">
     <title>HORIZONS</title>
 </head>
 
