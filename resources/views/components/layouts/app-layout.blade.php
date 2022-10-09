2d423b9db169836e5989df5aaf040b6e18b9a175
Misser <MiasserKuhail@yahoo.com>
2022-10-09 10:59:11 +0300

    ok

--- resources/views/components/layouts/app-layout.blade.php
+++ resources/views/components/layouts/app-layout.blade.php
@@ -31,8 +31,8 @@
     <div class="top-header py-2 container-fluid">
         <div class="container">
             <div class="row justify-content-center align-items-center justify-content-sm-between">
-                <div class="col-auto">
-                    <div class="contact-info">
+            {{-- <div class="col-auto">
+                  <div class="contact-info">
                         <a href="tel:+972599123456" class="me-2 d-inline-flex align-items-center">
                             <svg class="me-2" xmlns="http://www.w3.org/2000/svg" width="24.831" height="24"
                                 viewBox="0 0 24.831 24">
@@ -79,7 +79,8 @@
                             <span>mail@mail.com</span>
                         </a>
                     </div>
-                </div>
+                </div> --}}
+
                 <div class="col-auto">
                     <div class="links-social-media d-flex align-items-center">
                         <a href="" class="me-2">
@@ -92,8 +93,7 @@
                         </a>
                         <a href="" class="me-2">
                             <svg id="whatsapp-svgrepo-com_1_" data-name="whatsapp-svgrepo-com (1)"
-                                xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                viewBox="0 0 24 24">
+                                xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                                 <path id="Path_42" data-name="Path 42"
                                     d="M7.253,18.494l.724.423a8,8,0,1,0-2.893-2.893l.422.724-.653,2.4,2.4-.655ZM2,22l1.352-4.968A10,10,0,1,1,6.97,20.645ZM8.391,7.308a3.387,3.387,0,0,1,.4,0c.054,0,.108.01.162.016a.5.5,0,0,1,.393.249q.447,1.014.868,2.04a.607.607,0,0,1-.093.537,4.38,4.38,0,0,1-.263.372c-.113.145-.356.411-.356.411a.321.321,0,0,0-.061.265.885.885,0,0,0,.1.2l.059.1a6.335,6.335,0,0,0,1.02,1.268c.12.116.237.235.363.346a6.01,6.01,0,0,0,1.57,1l.005,0c.085.037.128.057.252.11a1.437,1.437,0,0,0,.191.066.35.35,0,0,0,.367-.13,11.677,11.677,0,0,1,.8-.934v0a.482.482,0,0,1,.378-.127.518.518,0,0,1,.177.04c.531.243,1.4.622,1.4.622l.582.261a.337.337,0,0,1,.19.265,1.93,1.93,0,0,1-.013.373,2.785,2.785,0,0,1-.188.733,1.155,1.155,0,0,1-.21.3,2.378,2.378,0,0,1-.33.288q-.062.046-.125.09-.187.118-.383.22a1.99,1.99,0,0,1-.833.23,5.155,5.155,0,0,1-.556.014c-.008,0-.568-.087-.568-.087A9.448,9.448,0,0,1,9.85,14.4c-.226-.2-.435-.413-.649-.626a9.51,9.51,0,0,1-1.97-2.742A3.47,3.47,0,0,1,6.9,9.62a2.729,2.729,0,0,1,.564-1.68,2.365,2.365,0,0,1,.261-.305,1.273,1.273,0,0,1,.294-.228.961.961,0,0,1,.371-.1Z"
                                     fill="none" />
@@ -103,8 +103,8 @@
                             <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
                                 width="24" height="24" viewBox="0 0 24 24">
                                 <defs>
-                                    <linearGradient id="linear-gradient" x1="0.5" y1="0.008"
-                                        x2="0.5" y2="0.998" gradientUnits="objectBoundingBox">
+                                    <linearGradient id="linear-gradient" x1="0.5" y1="0.008" x2="0.5"
+                                        y2="0.998" gradientUnits="objectBoundingBox">
                                         <stop offset="0" stop-color="#e09b3d" />
                                         <stop offset="0.3" stop-color="#c74c4d" />
                                         <stop offset="0.6" stop-color="#c21975" />
@@ -259,7 +259,7 @@
                                             transform="translate(-36.749 -228.41)" fill="#c71a1a" />
                                     </g>
                                 </svg>
-                                <span>City, Country</span>
+                                <span class="text-white">City, Country</span>
                             </a>
                         </div>
                         <div>
@@ -284,7 +284,7 @@
                                             transform="translate(-11.533 -2.543)" fill="#b51717" />
                                     </g>
                                 </svg>
-                                <span>+972599123456</span>
+                                <span class="text-white">+972599123456</span>
                             </a>
                         </div>
                         <div>
@@ -310,7 +310,7 @@
                                         </g>
                                     </g>
                                 </svg>
-                                <span>mail@mail.com</span>
+                                <span class="text-white">mail@mail.com</span>
                             </a>
                         </div>
                     </div>
